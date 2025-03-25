25b02fdce9e9857749be2217fddf560ed2d78ddc
r0227n <developryo@gmail.com>
2025-03-25 16:27:45 +0900

    add: convertToJsonFromYaml method test code

--- tools/update_lint_rules/lib/src/services/lint_rule_service.dart
+++ tools/update_lint_rules/lib/src/services/lint_rule_service.dart
@@ -124,22 +124,6 @@ class LintRuleService {
 
     final responseBody = await _appClient.read(url);
 
-    Map<String, dynamic> convertToJsonFromYaml(YamlMap yamlMap) {
-      dynamic jsonValue(dynamic value) {
-        return switch (value) {
-          YamlMap() => value.map(
-            (key, value) => MapEntry(key.toString(), jsonValue(value)),
-          ),
-          YamlList() => value.map(jsonValue).toList(),
-          _ => value,
-        };
-      }
-
-      return yamlMap.map(
-        (key, value) => MapEntry(key.toString(), jsonValue(value)),
-      );
-    }
-
     final yaml = loadYaml(responseBody);
 
     const nameKey = 'name';
@@ -183,6 +167,40 @@ class LintRuleService {
 
     return rules;
   });
+
+  @visibleForTesting
+  Map<String, dynamic> convertToJsonFromYaml(YamlMap yamlMap) {
+    dynamic jsonValue(dynamic value) {
+      return switch (value) {
+        YamlMap() => value.map(
+          (key, value) => MapEntry(key.toString(), jsonValue(value)),
+        ),
+        YamlList() => value.map(jsonValue).toList(),
+        _ => value,
+      };
+    }
+
+    return yamlMap.map(
+      (key, value) => MapEntry(key.toString(), jsonValue(value)),
+    );
+  }
+
+  @visibleForTesting
+  Map<String, dynamic> convertToJsonFromYaml(YamlMap yamlMap) {
+    dynamic jsonValue(dynamic value) {
+      return switch (value) {
+        YamlMap() => value.map(
+          (key, value) => MapEntry(key.toString(), jsonValue(value)),
+        ),
+        YamlList() => value.map(jsonValue).toList(),
+        _ => value,
+      };
+    }
+
+    return yamlMap.map(
+      (key, value) => MapEntry(key.toString(), jsonValue(value)),
+    );
+  }
 
   Future<bool> isFlutterOnlyRule(Rule rule) async {
     final containsFlutterOnlyRules =
