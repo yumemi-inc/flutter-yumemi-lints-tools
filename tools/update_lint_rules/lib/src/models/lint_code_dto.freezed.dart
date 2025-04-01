1d925cab44c6f8bfc207e1a893d64a9d5c30068d
r0227n <developryo@gmail.com>
2025-04-01 16:39:05 +0900

    add: Create DTO for LintCode

---
+++ tools/update_lint_rules/lib/src/models/lint_code_dto.freezed.dart
@@ -0,0 +1,371 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'lint_code_dto.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+LintCodeDto _$LintCodeDtoFromJson(Map<String, dynamic> json) {
+  return _LintCodeDto.fromJson(json);
+}
+
+/// @nodoc
+mixin _$LintCodeDto {
+  String get name => throw _privateConstructorUsedError;
+  String? get sharedName => throw _privateConstructorUsedError;
+  String? get problemMessage => throw _privateConstructorUsedError;
+  String? get correctionMessage => throw _privateConstructorUsedError;
+  Map<String, dynamic>? get state => throw _privateConstructorUsedError;
+  List<String>? get categories => throw _privateConstructorUsedError;
+  bool? get hasPublishedDocs => throw _privateConstructorUsedError;
+  String? get documentation => throw _privateConstructorUsedError;
+  String? get deprecatedDetails => throw _privateConstructorUsedError;
+  String? get todo => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $LintCodeDtoCopyWith<LintCodeDto> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $LintCodeDtoCopyWith<$Res> {
+  factory $LintCodeDtoCopyWith(
+          LintCodeDto value, $Res Function(LintCodeDto) then) =
+      _$LintCodeDtoCopyWithImpl<$Res, LintCodeDto>;
+  @useResult
+  $Res call(
+      {String name,
+      String? sharedName,
+      String? problemMessage,
+      String? correctionMessage,
+      Map<String, dynamic>? state,
+      List<String>? categories,
+      bool? hasPublishedDocs,
+      String? documentation,
+      String? deprecatedDetails,
+      String? todo});
+}
+
+/// @nodoc
+class _$LintCodeDtoCopyWithImpl<$Res, $Val extends LintCodeDto>
+    implements $LintCodeDtoCopyWith<$Res> {
+  _$LintCodeDtoCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? sharedName = freezed,
+    Object? problemMessage = freezed,
+    Object? correctionMessage = freezed,
+    Object? state = freezed,
+    Object? categories = freezed,
+    Object? hasPublishedDocs = freezed,
+    Object? documentation = freezed,
+    Object? deprecatedDetails = freezed,
+    Object? todo = freezed,
+  }) {
+    return _then(_value.copyWith(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      sharedName: freezed == sharedName
+          ? _value.sharedName
+          : sharedName // ignore: cast_nullable_to_non_nullable
+              as String?,
+      problemMessage: freezed == problemMessage
+          ? _value.problemMessage
+          : problemMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      correctionMessage: freezed == correctionMessage
+          ? _value.correctionMessage
+          : correctionMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      state: freezed == state
+          ? _value.state
+          : state // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>?,
+      categories: freezed == categories
+          ? _value.categories
+          : categories // ignore: cast_nullable_to_non_nullable
+              as List<String>?,
+      hasPublishedDocs: freezed == hasPublishedDocs
+          ? _value.hasPublishedDocs
+          : hasPublishedDocs // ignore: cast_nullable_to_non_nullable
+              as bool?,
+      documentation: freezed == documentation
+          ? _value.documentation
+          : documentation // ignore: cast_nullable_to_non_nullable
+              as String?,
+      deprecatedDetails: freezed == deprecatedDetails
+          ? _value.deprecatedDetails
+          : deprecatedDetails // ignore: cast_nullable_to_non_nullable
+              as String?,
+      todo: freezed == todo
+          ? _value.todo
+          : todo // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_LintCodeDtoCopyWith<$Res>
+    implements $LintCodeDtoCopyWith<$Res> {
+  factory _$$_LintCodeDtoCopyWith(
+          _$_LintCodeDto value, $Res Function(_$_LintCodeDto) then) =
+      __$$_LintCodeDtoCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String name,
+      String? sharedName,
+      String? problemMessage,
+      String? correctionMessage,
+      Map<String, dynamic>? state,
+      List<String>? categories,
+      bool? hasPublishedDocs,
+      String? documentation,
+      String? deprecatedDetails,
+      String? todo});
+}
+
+/// @nodoc
+class __$$_LintCodeDtoCopyWithImpl<$Res>
+    extends _$LintCodeDtoCopyWithImpl<$Res, _$_LintCodeDto>
+    implements _$$_LintCodeDtoCopyWith<$Res> {
+  __$$_LintCodeDtoCopyWithImpl(
+      _$_LintCodeDto _value, $Res Function(_$_LintCodeDto) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? sharedName = freezed,
+    Object? problemMessage = freezed,
+    Object? correctionMessage = freezed,
+    Object? state = freezed,
+    Object? categories = freezed,
+    Object? hasPublishedDocs = freezed,
+    Object? documentation = freezed,
+    Object? deprecatedDetails = freezed,
+    Object? todo = freezed,
+  }) {
+    return _then(_$_LintCodeDto(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      sharedName: freezed == sharedName
+          ? _value.sharedName
+          : sharedName // ignore: cast_nullable_to_non_nullable
+              as String?,
+      problemMessage: freezed == problemMessage
+          ? _value.problemMessage
+          : problemMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      correctionMessage: freezed == correctionMessage
+          ? _value.correctionMessage
+          : correctionMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      state: freezed == state
+          ? _value._state
+          : state // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>?,
+      categories: freezed == categories
+          ? _value._categories
+          : categories // ignore: cast_nullable_to_non_nullable
+              as List<String>?,
+      hasPublishedDocs: freezed == hasPublishedDocs
+          ? _value.hasPublishedDocs
+          : hasPublishedDocs // ignore: cast_nullable_to_non_nullable
+              as bool?,
+      documentation: freezed == documentation
+          ? _value.documentation
+          : documentation // ignore: cast_nullable_to_non_nullable
+              as String?,
+      deprecatedDetails: freezed == deprecatedDetails
+          ? _value.deprecatedDetails
+          : deprecatedDetails // ignore: cast_nullable_to_non_nullable
+              as String?,
+      todo: freezed == todo
+          ? _value.todo
+          : todo // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_LintCodeDto implements _LintCodeDto {
+  _$_LintCodeDto(
+      {required this.name,
+      required this.sharedName,
+      required this.problemMessage,
+      required this.correctionMessage,
+      required final Map<String, dynamic>? state,
+      required final List<String>? categories,
+      required this.hasPublishedDocs,
+      required this.documentation,
+      required this.deprecatedDetails,
+      required this.todo})
+      : _state = state,
+        _categories = categories;
+
+  factory _$_LintCodeDto.fromJson(Map<String, dynamic> json) =>
+      _$$_LintCodeDtoFromJson(json);
+
+  @override
+  final String name;
+  @override
+  final String? sharedName;
+  @override
+  final String? problemMessage;
+  @override
+  final String? correctionMessage;
+  final Map<String, dynamic>? _state;
+  @override
+  Map<String, dynamic>? get state {
+    final value = _state;
+    if (value == null) return null;
+    if (_state is EqualUnmodifiableMapView) return _state;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableMapView(value);
+  }
+
+  final List<String>? _categories;
+  @override
+  List<String>? get categories {
+    final value = _categories;
+    if (value == null) return null;
+    if (_categories is EqualUnmodifiableListView) return _categories;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(value);
+  }
+
+  @override
+  final bool? hasPublishedDocs;
+  @override
+  final String? documentation;
+  @override
+  final String? deprecatedDetails;
+  @override
+  final String? todo;
+
+  @override
+  String toString() {
+    return 'LintCodeDto(name: $name, sharedName: $sharedName, problemMessage: $problemMessage, correctionMessage: $correctionMessage, state: $state, categories: $categories, hasPublishedDocs: $hasPublishedDocs, documentation: $documentation, deprecatedDetails: $deprecatedDetails, todo: $todo)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_LintCodeDto &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.sharedName, sharedName) ||
+                other.sharedName == sharedName) &&
+            (identical(other.problemMessage, problemMessage) ||
+                other.problemMessage == problemMessage) &&
+            (identical(other.correctionMessage, correctionMessage) ||
+                other.correctionMessage == correctionMessage) &&
+            const DeepCollectionEquality().equals(other._state, _state) &&
+            const DeepCollectionEquality()
+                .equals(other._categories, _categories) &&
+            (identical(other.hasPublishedDocs, hasPublishedDocs) ||
+                other.hasPublishedDocs == hasPublishedDocs) &&
+            (identical(other.documentation, documentation) ||
+                other.documentation == documentation) &&
+            (identical(other.deprecatedDetails, deprecatedDetails) ||
+                other.deprecatedDetails == deprecatedDetails) &&
+            (identical(other.todo, todo) || other.todo == todo));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      name,
+      sharedName,
+      problemMessage,
+      correctionMessage,
+      const DeepCollectionEquality().hash(_state),
+      const DeepCollectionEquality().hash(_categories),
+      hasPublishedDocs,
+      documentation,
+      deprecatedDetails,
+      todo);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_LintCodeDtoCopyWith<_$_LintCodeDto> get copyWith =>
+      __$$_LintCodeDtoCopyWithImpl<_$_LintCodeDto>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_LintCodeDtoToJson(
+      this,
+    );
+  }
+}
+
+abstract class _LintCodeDto implements LintCodeDto {
+  factory _LintCodeDto(
+      {required final String name,
+      required final String? sharedName,
+      required final String? problemMessage,
+      required final String? correctionMessage,
+      required final Map<String, dynamic>? state,
+      required final List<String>? categories,
+      required final bool? hasPublishedDocs,
+      required final String? documentation,
+      required final String? deprecatedDetails,
+      required final String? todo}) = _$_LintCodeDto;
+
+  factory _LintCodeDto.fromJson(Map<String, dynamic> json) =
+      _$_LintCodeDto.fromJson;
+
+  @override
+  String get name;
+  @override
+  String? get sharedName;
+  @override
+  String? get problemMessage;
+  @override
+  String? get correctionMessage;
+  @override
+  Map<String, dynamic>? get state;
+  @override
+  List<String>? get categories;
+  @override
+  bool? get hasPublishedDocs;
+  @override
+  String? get documentation;
+  @override
+  String? get deprecatedDetails;
+  @override
+  String? get todo;
+  @override
+  @JsonKey(ignore: true)
+  _$$_LintCodeDtoCopyWith<_$_LintCodeDto> get copyWith =>
+      throw _privateConstructorUsedError;
+}
